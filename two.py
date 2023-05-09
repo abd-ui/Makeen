e5e665dc5db1a7cb767d01830e02114cf2b31d57
Muaeen Gharib <130491012+Muaeen@users.noreply.github.com>
2023-05-09 13:27:37 +0400

    Update two.py

--- two.py
+++ two.py
@@ -6,7 +6,7 @@ r = 6
 volume = h*pi*(r**2)
 area = round(2*pi*r*(r+h))
 
-print("Heigth of cylinder: ", str(h))
+print("Heigth of cylinder: ", stjhgjhr(h))
 print("Radius of cylnider: ", str(r))
 print("Volume is: ", str("%.2f" % volume))
 print("Surface area is: ", str("%.2f" % area))
