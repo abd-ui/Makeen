b4ca405217dcade9cfc58da50e63334a6952da01
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2023-05-19 19:44:54 +0400

    chat gpt

---
+++ chatGPT/chat.py
@@ -0,0 +1,20 @@
+import openai
+
+openai.api_key = "YOUR API KEY HERE"
+
+user_input = input()
+
+while user_input != '':
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_input},
+        ]
+    )
+
+    message = response.choices[0]['message']
+    print("{}: {}".format(message['role'], message['content']))
+
+    user_input = input()
