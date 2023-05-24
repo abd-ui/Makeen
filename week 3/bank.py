74524359963a89e3c5ed053ae5caacba2ef871a2
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2023-05-24 21:47:13 +0400

    used threading to make all three employees work

--- week 3/bank.py
+++ week 3/bank.py
@@ -1,5 +1,6 @@
 import random
 import time
+import threading
 
 
 class Customer:
@@ -11,48 +12,50 @@ class Customer:
         return self.name
 
 
-class Employee:
-    def __init__(self, name):
-        self.name = name
-        self.is_busy = False
-
-    def serve_customer(self, customer):
-        print(self.name + " is serving " +
-              customer.name + " for " + str(customer.processing_time) + " seconds")
-        self.is_busy = True
-        time.sleep(customer.processing_time)
-
-
 customers = []
-
-employees = [
-    Employee("Employee 1"),
-    Employee("Employee 2"),
-    Employee("Employee 3"),
-]
-
-
-def check_all_employees(employees):
-    busy = False
-    for employee in employees:
-        busy = employee.is_busy
-    return busy
-
-
 for i in range(10):
     processing_time = random.randint(1, 5)
     customers.insert(0, Customer("Customer " + str(i + 1), processing_time))
 
-while customers:
-    if not check_all_employees(employees):
+
+def employee1(customers):
+    while customers:
         customer = customers.pop()
+        print("employee1 is serving " +
+              customer.name + " for " + str(customer.processing_time) + " seconds\n")
+        time.sleep(customer.processing_time)
 
-        for employee in employees:
-            if not employee.is_busy:
-                employee.serve_customer(customer)
-                break
-    else:
-        for employee in employees:
-            employee.is_busy = False
 
-print("All customers have been served")
+def employee2(customers):
+    while customers:
+        customer = customers.pop()
+        print("employee2 is serving " +
+              customer.name + " for " + str(customer.processing_time) + " seconds\n")
+        time.sleep(customer.processing_time)
+
+
+def employee3(customers):
+    while customers:
+        customer = customers.pop()
+        print("employee3 is serving " +
+              customer.name + " for " + str(customer.processing_time) + " seconds\n")
+        time.sleep(customer.processing_time)
+
+
+x = threading.Thread(target=employee1, args=(customers,))
+x.start()
+y = threading.Thread(target=employee2, args=(customers,))
+y.start()
+z = threading.Thread(target=employee3, args=(customers,))
+z.start()
+
+
+x.join()
+y.join()
+z.join()
+
+
+print("All customers has been served")
+print(threading.active_count())
+print(threading.enumerate())
+print(time.perf_counter())
