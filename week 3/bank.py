01ca1c6b11cb2b8d557c062b19a9a5c55a17c32c
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2023-05-24 22:25:44 +0400

    timer works but catch exceptions

--- week 3/bank.py
+++ week 3/bank.py
@@ -1,6 +1,11 @@
 import random
 import time
 import threading
+import signal
+import sys
+
+
+signal.alarm(30)
 
 
 class Customer:
@@ -13,35 +18,47 @@ class Customer:
 
 
 customers = []
-for i in range(10):
-    processing_time = random.randint(1, 5)
-    customers.insert(0, Customer("Customer " + str(i + 1), processing_time))
+
+
+def add_customers(customers):
+    i = 0
+    while True:
+        processing_time = random.randint(1, 10)
+        customers.insert(0, Customer(
+            "Customer " + str(i + 1), processing_time))
+        time.sleep(random.randint(1, 10))
+        i += 1
 
 
 def employee1(customers):
-    while customers:
-        customer = customers.pop()
-        print("employee1 is serving " +
-              customer.name + " for " + str(customer.processing_time) + " seconds\n")
-        time.sleep(customer.processing_time)
+    while True:
+        while customers:
+            customer = customers.pop()
+            print("employee1 is serving " +
+                  customer.name + " for " + str(customer.processing_time) + " seconds\n")
+            time.sleep(customer.processing_time)
 
 
 def employee2(customers):
-    while customers:
-        customer = customers.pop()
-        print("employee2 is serving " +
-              customer.name + " for " + str(customer.processing_time) + " seconds\n")
-        time.sleep(customer.processing_time)
+    while True:
+        while customers:
+            customer = customers.pop()
+            print("employee2 is serving " +
+                  customer.name + " for " + str(customer.processing_time) + " seconds\n")
+            time.sleep(customer.processing_time)
 
 
 def employee3(customers):
-    while customers:
-        customer = customers.pop()
-        print("employee3 is serving " +
-              customer.name + " for " + str(customer.processing_time) + " seconds\n")
-        time.sleep(customer.processing_time)
+    while True:
+        while customers:
+            customer = customers.pop()
+            print("employee3 is serving " +
+                  customer.name + " for " + str(customer.processing_time) + " seconds\n")
+            time.sleep(customer.processing_time)
 
 
+a = threading.Thread(target=add_customers, args=(customers,))
+a.start()
 x = threading.Thread(target=employee1, args=(customers,))
 x.start()
 y = threading.Thread(target=employee2, args=(customers,))
@@ -49,7 +66,7 @@ y.start()
 z = threading.Thread(target=employee3, args=(customers,))
 z.start()
 
-
+a.join()
 x.join()
 y.join()
 z.join()
