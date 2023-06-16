b989c45b9c61405f62f8207bbdcf612edaf7838e
Abdullah Alwahaibi <19574962@students.ltu.edu.au>
2023-06-16 14:16:53 +0400

    added data

--- SQL/leagueManagmentSystem.sql
+++ SQL/leagueManagmentSystem.sql
@@ -125,3 +125,81 @@ CREATE TABLE ClubSeason (
     CONSTRAINT position_positive CHECK (Position > 0)
 );
 
+
+-------------------------------------------------------------------------------------------
+
+-- Club table
+INSERT INTO Club (ClubID, ClubName, ClubLocation, ClubValue, CurrentPosition, LeagueID) VALUES
+('MU1', 'Manchester United', 'Manchester, England', 1000000, 1, 'PL1'),
+('AR2', 'Arsenal', 'London, England', 750000, 2, 'PL1'),
+('CH3', 'Chelsea', 'London, England', 500000, 3, 'PL1'),
+('PSG1', 'Paris Saint-Germain', 'Paris, France', 2000000, 1, 'L1'),
+('MC2', 'Manchester City', 'Manchester, England', 1500000, 4, 'PL1'),
+('RM3', 'Real Madrid', 'Madrid, Spain', 1250000, 3, 'LL1'),
+('BV4', 'Bayern Munich', 'Munich, Germany', 1000000, 1, 'BL1'),
+('AC5', 'AC Milan', 'Milan, Italy', 750000, 1, 'SA1');
+
+
+-- Player table
+INSERT INTO Player (PlayerID, PlayerName, PlayerAge, Nationality, ClubID, PlayerSalary, GoalsScored) VALUES
+('CR7', 'Cristiano Ronaldo', 37, 'Portugal', 'MU1', 5000000, 20),
+('LM10', 'Lionel Messi', 35, 'Argentina', 'AR2', 4000000, 30),
+('NJR', 'Neymar', 30, 'Brazil', 'CH3', 3000000, 25),
+('MBappe', 'Kylian Mbappe', 23, 'France', 'PSG1', 4500000, 25),
+('Haaland', 'Erling Haaland', 21, 'Norway', 'MC2', 5000000, 30),
+('Modric', 'Luka Modric', 37, 'Croatia', 'RM3', 3500000, 15),
+('Benzema', 'Karim Benzema', 34, 'France', 'RM3', 4000000, 25),
+('Salah', 'Mohamed Salah', 29, 'Egypt', 'MC2', 4000000, 20),
+('Mane', 'Sadio Mane', 30, 'Senegal', 'MC2', 4000000, 20),
+('KDB', 'Kevin De Bruyne', 31, 'Belgium', 'MC2', 4500000, 25);
+
+
+-- Match table
+INSERT INTO Match (MatchID, MatchDate, HomeClubScore, AwayClubScore, Attendance, HomeClubID, AwayClubID, RefereeID, StadiumID) VALUES
+('MU1-AR2', '2023-06-15', 1, 0, 10000, 'MU1', 'AR2', 'MO1', 'OT1'),
+('CH3-AR2', '2023-06-17', 2, 1, 20000, 'CH3', 'AR2', 'JM1', 'SB1');
+
+-- Referee table
+INSERT INTO Referee (RefereeID, RefereeName, Nationality, RefereeSalary) VALUES
+('MD1', 'Mike Dean', 'England', 200000),
+('JM1', 'Jon Moss', 'England', 150000),
+('MO1', 'Micheal Oliver', 'England', 100000);
+
+-- Stadium table
+INSERT INTO Stadium (StadiumID, StadiumName, StadiumLocation, Capacity) VALUES
+('OT1', 'Old Trafford', 'Manchester, England', 75000),
+('ES1', 'Emirates Stadium', 'London, England', 60000),
+('SB1', 'Stamford Bridge', 'London, England', 40000);
+
+-- Season table
+INSERT INTO Season (SeasonID, StartYear, EndYear) VALUES
+('S1', 2022, 2023),
+('S2', 2023, 2024);
+
+-- League table
+INSERT INTO League (LeagueID, LeagueName) VALUES 
+('L1', 'Legue 1'),
+('SA1', 'Seria A'),
+('LL1', 'La Liga'),
+('PL1', 'Premier League'),
+('LA1', 'La Liga'),
+('BL1', 'Bundesliga');
+
+-- Manager table
+INSERT INTO Manager (ManagerID, ManagerName, Nationality, ClubID) VALUES
+('OGS1', 'Ole Gunnar Solskjaer', 'Norway', 'MU1'),
+('MA1', 'Mike Arteta', 'Spain', 'AR2'),
+('TT1', 'Thomas Tuchel', 'Germany', 'CH3');
+
+-- ClubSeason table
+INSERT INTO ClubSeason (ClubID, SeasonID, LeagueID, Position) VALUES
+('MU1', 'S1', 'PL1', 1),
+('AR2', 'S1', 'PL1', 2),
+('CH3', 'S1', 'PL1', 3);
+
+
+INSERT INTO Club (ClubID, ClubName, ClubLocation) VALUES
+('MU1', 'Manchester United', 'Manchester, England')
+
+SELECT * FROM player;
+
